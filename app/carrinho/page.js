17792551f3a8e77038b243a3c713416fc3648e5c
Quiz Java- Futebol Brasileiro <guiilhermecaua13@gmail.com>
2026-06-11 22:01:03 -0300

    Criando a pagina carrinho

---
+++ app/carrinho/page.js
@@ -0,0 +1,8 @@
+export default function Carrinho() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Seu Carrinho</h2>
+      <p>O carrinho está vazio no momento.</p>
+    </div>
+  );
+}
