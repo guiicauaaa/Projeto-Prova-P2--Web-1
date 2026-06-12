ecacbb190e0a9d678aceaa8748e89c622a698f5c
Quiz Java- Futebol Brasileiro <guiilhermecaua13@gmail.com>
2026-06-11 22:00:17 -0300

    Criando a pagina sobre

---
+++ app/sobre/page.js
@@ -0,0 +1,9 @@
+export default function Sobre() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Sobre o Projeto</h2>
+      <p>Este catálogo foi desenvolvido como avaliação da disciplina de Programação Web I.</p>
+      <p>Utilizamos Next.js, React e CSS Modules para criar uma experiência rápida e interativa.</p>
+    </div>
+  );
+}
