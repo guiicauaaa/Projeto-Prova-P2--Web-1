fb682d35e2f81d86d0c571cf06b60428f8608ce0
Quiz Java- Futebol Brasileiro <guiilhermecaua13@gmail.com>
2026-06-11 21:57:04 -0300

    Criando o Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2026 Catálogo Interativo. Avaliação de Programação Web I.</p>
+    </footer>
+  );
+}
